6d81e9ab4f539c694341ad80e88313190366b094
Sohaib Arif <arif_sohaib@outlook.com>
2018-07-15 14:23:27 -0400

    added proper folder labeling when extracting images from videos

--- datasetSearch/extract_images.py
+++ datasetSearch/extract_images.py
@@ -1,18 +1,26 @@
 import cv2
 
 from glob import glob
+import os
 
-
-def frame_capture(vid_file):
+def frame_capture(vid_file, label):
     cap = cv2.VideoCapture(vid_file)
     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    file_name_idx = vid_file.find(label)+1
+    print("label:{}".format(label))
+    print(file_name_idx)
+    file_name = vid_file[file_name_idx+len(label):]
+    file_name = file_name[:-4]
+    print("file_name:{}".format(file_name))
     frame_count = 0
     print("getting {} images".format(num_frames))
     try:
         while True:
             ret, frame = cap.read()
             cv2.imshow("frame1", frame)
-            cv2.imwrite("../images/{}_{}.jpg".format(vid_file, frame_count), frame)
+            cv2.imwrite("./images/{}/{}_{}.jpg".format(label,file_name, frame_count), frame)
+            #uncomment for debugging
+            # print(os.path.abspath("./images/{}/{}_{}.jpg".format(label,file_name, frame_count)))
             frame_count += 1
             if ret == False:
                 break
@@ -21,14 +29,19 @@ def frame_capture(vid_file):
                 break
     except:
         print("got {} images out of {}".format(frame_count, num_frames))
+video_labels = glob("./videos/*")
 
-video_files = glob("../videos/*.MOV")
-print(video_files)
+for lbl in video_labels:
+    label = lbl[9:]
+    print(label)
+    video_files = glob("./videos/{}/*.MOV".format(label))
+    print(video_files)
+    print("extracting {}".format(label))
+    print("abs path: {}".format(os.path.abspath(lbl)))
+    if not os.path.exists("./images/{}".format(label)):
+        os.mkdir("./images/{}".format(label))
+    for i in range(len(video_files)):
+        print("processing video:{}".format(video_files[i]))
+        frame_capture(video_files[i], label)
 
-for i in range(len(video_files)):
-    print("processing video:{}".format(video_files[i]))
-    try:
-        frame_capture(video_files[i])
-    except:
-        break
 cv2.destroyAllWindows()
