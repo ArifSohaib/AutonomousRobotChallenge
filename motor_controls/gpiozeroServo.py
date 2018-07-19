033590a96a39b34b3ce0570add86c12c4016e19d
Sohaib Arif <arif_sohaib@outlook.com>
2018-07-19 13:35:27 -0400

    added global variables for servos

--- motor_controls/gpiozeroServo.py
+++ motor_controls/gpiozeroServo.py
@@ -1,6 +1,15 @@
 from gpiozero import Servo
 from time import sleep
 
+#global definations
+myGPIO = 18
+myGPIO2 = 23
+correction = 0.45
+maxPW = (2.0+correction)/1000
+minPW = (1.0-correction)/1000
+
+myServo1 = Servo(myGPIO, min_pulse_width=minPW, max_pulse_width=maxPW)
+myServo2 = Servo(myGPIO2, min_pulse_width=minPW, max_pulse_width=maxPW)
 
 def forward(myServo1, myServo2):
     myServo1.min()
@@ -18,14 +27,6 @@ def backward(myServo1, myServo2):
 
 
 def main():
-    myGPIO = 18
-    myGPIO2 = 23
-    correction = 0.45
-    maxPW = (2.0+correction)/1000
-    minPW = (1.0-correction)/1000
-
-    myServo1 = Servo(myGPIO, min_pulse_width=minPW, max_pulse_width=maxPW)
-    myServo2 = Servo(myGPIO2, min_pulse_width=minPW, max_pulse_width=maxPW)
     forward(myServo1, myServo2)
 
 if __name__ == "__main__":
