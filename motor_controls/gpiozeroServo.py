35960f17c2216e1a8eeaab38f89e17e0f7f33d55
Sohaib Arif <arif_sohaib@outlook.com>
2018-07-19 13:56:28 -0400

    turned gpiozeroServo into class MotorControls

--- motor_controls/gpiozeroServo.py
+++ motor_controls/gpiozeroServo.py
@@ -1,33 +1,35 @@
 from gpiozero import Servo
 from time import sleep
 
-#global definations
-myGPIO = 18
-myGPIO2 = 23
-correction = 0.45
-maxPW = (2.0+correction)/1000
-minPW = (1.0-correction)/1000
+class MotorControls:
+    def __init__(self, pin1=18, pin2=23, correction=0.45):
+        self.myGPIO1 = pin1
+        self.myGPIO2 = pin2
+        self.correction = correction
+        self.maxPW = (2.0+correction)/1000
+        self.minPW = (1.0-correction)/1000
 
-myServo1 = Servo(myGPIO, min_pulse_width=minPW, max_pulse_width=maxPW)
-myServo2 = Servo(myGPIO2, min_pulse_width=minPW, max_pulse_width=maxPW)
+        self.myServo1 = Servo(self.myGPIO1, min_pulse_width=self.minPW, max_pulse_width=self.maxPW)
+        self.myServo2 = Servo(self.myGPIO2, min_pulse_width=self.minPW, max_pulse_width=self.maxPW)
 
-def forward(myServo1=myServo1, myServo2=myServo2):
-    myServo1.min()
-    myServo2.max()
-    sleep(0.5)
+    def forward(self):
+        self.myServo1.min()
+        self.myServo2.max()
+        sleep(0.5)
 
 
 
-def backward(myServo1=myServo1, myServo2=myServo2):
-    myServo1.max()
-    myServo2.max()
-    sleep(1)
+    def backward(self):
+        self.myServo1.max()
+        self.myServo2.max()
+        sleep(1)
 
-def turn1(myServo1=myServo1, myServo2=myServo2):
-    myServo1.max()
-    myServo2.max()
-    sleep(1)
+    def turn1(self):
+        self.myServo1.max()
+        self.myServo2.max()
+        sleep(1)
 
 
 if __name__ == "__main__":
-    forward()
+    motor = MotorControls()
+    motor.forward()
