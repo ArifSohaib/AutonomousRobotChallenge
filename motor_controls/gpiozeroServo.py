f365f2eb8c149de285244188231cd7271c21f989
Sohaib Arif <arif_sohaib@outlook.com>
2018-07-19 16:28:29 -0400

    tried to fix initialization of class

--- motor_controls/gpiozeroServo.py
+++ motor_controls/gpiozeroServo.py
@@ -9,28 +9,38 @@ class MotorControls:
         self.maxPW = (2.0+correction)/1000
         self.minPW = (1.0-correction)/1000
 
+    def __initServo(self):
         self.myServo1 = Servo(self.myGPIO1, min_pulse_width=self.minPW, max_pulse_width=self.maxPW)
         self.myServo2 = Servo(self.myGPIO2, min_pulse_width=self.minPW, max_pulse_width=self.maxPW)
 
     def forward(self):
+        if self.myServo1 == None:
+            self.__initServo()
         print("forward called")
         self.myServo1.min()
         self.myServo2.max()
         sleep(0.5)
 
     def backward(self):
+        if self.myServo1 == None:
+            self.__initServo()
         print("backward called")
         self.myServo1.min()
         self.myServo2.min()
+        
         sleep(0.5)
 
     def turn1(self):
+        if self.myServo1 == None:
+            self.__initServo()
         print("turn1 called")
         self.myServo1.max()
         self.myServo2.max()
         sleep(0.5)
 
     def turn2(self):
+        if self.myServo1 == None:
+            self.__initServo()
         print("turn2 called")
         self.myServo1.min()
         self.myServo2.min()
