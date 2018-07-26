631bb3403b1d65f0fbbf7d223bdaf78efbf2461c
Sohaib Arif <arif_sohaib@outlook.com>
2018-07-26 11:57:12 -0400

    fixed pi video stream reading

--- videoClassification/read_pi_cam_stream.py
+++ videoClassification/read_pi_cam_stream.py
@@ -1,26 +1,21 @@
 import cv2
-import numpy as np 
-# from bs4 import BeautifulSoup
-import urllib3
 
-http = urllib3.PoolManager()
-
-url = 'http://raspberrypi:8160/video'
-print("connected")
+cap = cv2.VideoCapture('http://raspberrypi:8160')
+if not cap.isOpened():
+    print('VideoCapture not opened')
+    exit(-1)
 
 while True:
-    response = http.request('GET', url)
-    print("got response")
-    print(response.data)
-    imgNp=np.array(bytearray(response.data), dtype=np.uint8)
-    print(type(imgNp))
-    print(imgNp.shape)
-    img = cv2.imdecode(imgNp,-1)
-    print(imgNp.shape)
-    print("working")
-    break
-    # print(type(img))
-    # cv2.imshow('test', img)
-    # if cv2.waitKey(0) and 0xFF == ord('q'):
-    #     cv2.destroyAllWindows()
-    #     break
+    ret, frame = cap.read()
+
+    if not ret:
+        print('frame empty')
+        break
+
+    cv2.imshow('image', frame)
+
+    if cv2.waitKey(1)&0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
