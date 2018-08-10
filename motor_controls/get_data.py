0dd322a9385902d7af379dd891d0b478539f073a
Sohaib Arif <arif_sohaib@outlook.com>
2018-08-10 11:34:55 -0400

    added script to create training data

--- motor_controls/get_data.py
+++ motor_controls/get_data.py
@@ -1,10 +1,15 @@
 # import curses and GPIO
 import curses
-# from gpiozeroServo import MotorControls
 from gpioServo import MotorControls
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import numpy as np
 motor = MotorControls()
-
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640,480))
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
 screen = curses.initscr()
@@ -12,32 +17,40 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
-keyRec = file('key_strokes.txt','w+')
+keyRec = open('key_strokes.txt','w+')
 
-
+train_data = []
+count = 0
 try:
-        while True:   
+    while True:
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            image_np = np.array(frame.array)
+            rawCapture.truncate(0)
+            
             char = screen.getch()
             if char == ord('q'):
                 motor.end()
                 break
             elif char == curses.KEY_UP:
                 motor.forward()
-                keyRec.write('1,0,0,0\n')
+                key = '1,0,0,0\n'
 
             elif char == curses.KEY_DOWN:
                 motor.stop()
-                keyRec.write('0,1,0,0\n')
+                key = '0,1,0,0\n'
                 
-
             elif char == curses.KEY_RIGHT:
                 motor.turn1()
-                keyRec.write('0,0,1,0\n')
+                key = '0,0,1,0\n'
                 
             elif char == curses.KEY_LEFT:
                 motor.turn2()
-                keyRec.write('0,0,0,1\n')
-            
+                key = '0,0,0,1\n'
+            val_dict = {"inp":key, "image":image_np}
+            train_data.append(val_dict)
+            keyRec.write(key)
+            if count % 500 == 0:
+                np.save("train_data.npy", train_data)
 finally:
     #Close down curses properly, inc turn echo back on!
     keyRec.close()
