f1a819754c5d7de49fcffafd9e46ea56127bd8a2
Sohaib Arif <arif_sohaib@outlook.com>
2018-08-16 10:52:31 -0400

    added key to close program

--- motor_controls/keyboardArduino.py
+++ motor_controls/keyboardArduino.py
@@ -12,31 +12,33 @@ keyRec = open('key_strokes.txt','w+')
 ser = serial.Serial("/dev/ttyUSB0", "9600")
 
 try:
-        while True:   
-            char = screen.getch()
-            if char == ord('q'):
-                ser.write('5')
-                #one-hot format ['up', 'down', 'left', 'right','pause']
-                key = [0,0,0,0,1]
-            elif char == ord('w'):
-                ser.write('1')
-                key = [1,0,0,0,0]
+    while True:   
+        char = screen.getch()
+        if char == ord('q'):
+            ser.write('5')
+            #one-hot format ['up', 'down', 'left', 'right','pause']
+            key = [0,0,0,0,1]
+        elif char == ord('w'):
+            ser.write('1')
+            key = [1,0,0,0,0]
 
-            elif char == ord('s'):
-                ser.write('2')
-                key = [0,1,0,0,0]
+        elif char == ord('s'):
+            ser.write('2')
+            key = [0,1,0,0,0]
 
-            elif char == ord('a'):
-                ser.write('3')
-                key = [0,0,1,0,0]
+        elif char == ord('a'):
+            ser.write('3')
+            key = [0,0,1,0,0]
                 
-            elif char == ord('d'):
-                ser.write('4')
-                key = [0,0,0,1,0]
-            else:
-                print("unknown command, pausing, key not recorded")
-                ser.write('5')
-            keyRec.write(str(key)+"\n")
+        elif char == ord('d'):
+            ser.write('4')
+            key = [0,0,0,1,0]
+        elif char == ord('x'):
+            break
+        else:
+            print("unknown command, pausing, key not recorded")
+            ser.write('5')
+        keyRec.write(str(key)+"\n")
 finally:
     #Close down curses properly, inc turn echo back on!
     keyRec.close()
