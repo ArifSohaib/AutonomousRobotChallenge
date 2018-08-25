265f0f0ea3bdad796e34b221f3625ec4a3433ec4
Sohaib Arif <arif_sohaib@outlook.com>
2018-08-25 11:53:18 -0400

    added tensorflow dataset loading to test training file

--- motor_controls/test_training_file.py
+++ motor_controls/test_training_file.py
@@ -4,6 +4,26 @@ data= np.load("train_data_wcc.npy", encoding='latin1')
 
 import matplotlib.pyplot as plt 
 import cv2
+
 img = plt.imshow(data[100]["image"][:,:,::-1])
-print(type(img))
+
 plt.show()
+
+import tensorflow as tf 
+images = []
+labels = []
+for i in range(len(data)):
+    images.append(data[i]['image'])
+    labels.append(data[i]['input'])
+
+images = np.array(images)
+labels = np.array(labels)
+features_placeholder = tf.placeholder(images.dtype, images.shape)
+labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
+
+dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
+iterator = dataset.make_initializable_iterator()
+sess = tf.Session()
+sess.run(iterator.initializer, feed_dict={features_placeholder: images,
+                                          labels_placeholder: labels})
+sess.close()
