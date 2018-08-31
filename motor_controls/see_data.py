a8e6473c5b1c9e0b9beab84edd544e8114c12733
Sohaib Arif <arif_sohaib@outlook.com>
2018-08-31 12:09:43 -0400

    fixed some typos in see_data.py

--- motor_controls/see_data.py
+++ motor_controls/see_data.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 
 fig = plt.figure()
 
-data = np.load("train_data_wcc2.npy", encoding="latin1")
+data = np.load("train_data.npy", encoding="latin1")
 # each frame
 ims = []
 for i in range(len(data)):
@@ -12,7 +12,7 @@ for i in range(len(data)):
     ims.append([im])
 
 
-ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+ani = animation.ArtistAnimation(fig, ims, interval=60, blit=True,
                                 repeat_delay=1000)
 
 # ani.save('dynamic_images.mp4')
@@ -22,7 +22,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load('train_data_wcc2.npy',encoding='latin1')
 for i in range(len(data)):
-    cv2.imshow("imgage",data[i]['image'])
+    cv2.imshow("image",data[i]['image'])
     
