9b711df49ad1f260396f6f95def0dac5c48b09cf
Sohaib Arif <arif_sohaib@outlook.com>
2018-09-01 08:23:01 -0400

    changed model to remove dense layer and added code to save model

--- motor_controls/drive_model.py
+++ motor_controls/drive_model.py
@@ -3,7 +3,10 @@ from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import Convolution2D, MaxPool2D, Reshape
 from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+import os
 import numpy as np
+
 def default_linear():
     "fully connected version of the default linear model"
     img_in = Input(shape=(224, 224, 3), name='img_in')
@@ -16,15 +19,11 @@ def default_linear():
     x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='elu')(x) #output shape 12x12
     x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='elu')(x) #output shape 10x10
     x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='elu')(x) #output shape 5x5
-    x = Convolution2D(filters=100, kernel_size=(3,3), strides=(2, 2), activation='elu')(x)
+    x = Convolution2D(filters=5, kernel_size=(3,3), strides=(2, 2), activation='elu')(x)
     # x = Reshape([None, 100])(x)
-    x = Flatten(name='flattened')(x)
-    # x = Dense(units=100, activation='linear')(x)
-    # x = Dropout(rate=.1)(x)
-    # x = Dense(units=50, activation='linear')(x)
-    # x = Dropout(rate=.1)(x)
-    # categorical output of the angle
-    control_out = Dense(units=4, activation='relu', name='control_out')(x)
+    # x = Flatten(name='flattened')(x)
+    control_out = Flatten(name='control_out')(x)
+    # control_out = Dense(units=4, activation='relu', name='control_out')(x)
 
     # continous output of throttle for later possibly
     # throttle_out = Dense(units=1, activation='linear', name='throttle_out')(x)
@@ -37,7 +36,7 @@ def default_linear():
     return model
 
 def main():
-    data= np.load("train_data_wcc.npy", encoding='latin1')
+    data= np.load("train_data.npy", encoding='latin1')
 
     images = []
     labels = []
@@ -49,7 +48,15 @@ def main():
     labels = np.array(labels)
 
     model = default_linear()
-    model.fit(images, labels, batch_size=32, epochs=100)
+    print(model.summary())
+    checkpoint_path = "training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Create checkpoint callback
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, 
+                                                 save_weights_only=True,
+                                                 verbose=1)
+    model.fit(images, labels, batch_size=32, epochs=100, callbacks = [cp_callback])
 
     #test model 
     preds = model.predict(images[:10])
