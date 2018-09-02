bdede6f7439dec157c72f83f36670ab02181c8a9
Sohaib Arif <arif_sohaib@outlook.com>
2018-09-02 01:39:38 -0400

    changed testing file

--- motor_controls/test_training_file.py
+++ motor_controls/test_training_file.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from drive_model import default_linear
 import numpy as np
 import serial
-#import curses
+
 model = default_linear()
 checkpoint_path = "./training_1/model.h5"
 model.load_weights(checkpoint_path)
@@ -22,108 +22,25 @@ try:
     rawCapture = PiRGBArray(camera, size=(224,224))
     piCam = True
 except:
-    print("Pi camera does not exist, using USB camera")
+    print("Pi camera does not exist")
 
 
-#screen = curses.initscr()
-#curses.noecho() 
-#curses.cbreak()
-#screen.keypad(True)
 
 keyRec = open('key_strokes.txt','w+')
 
 train_data = []
 
-try:
-    while True:
-        distString = serLidar.readline()
-        dist = 1000
-        try:
-            dist = int(distString.decode("utf-8"))
-        except:
-            print("can't convert dist")
-        if piCam == True:
-            for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-                image_np = np.array(frame.array)
+while True:
+    distString = serLidar.readline()
+    dist = 1000
+    try:
+        dist = int(distString.decode("utf-8"))
+    except:
+        print("can't convert dist")
+    if piCam == True:
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            image_np = np.array(frame.array)
                 
-                rawCapture.truncate(0)
-                
-                #char = screen.getch()
-                key = [0,0,0,0,1]
-                if np.argmax(model.predict(image_np)) == 0 and dist > 100:
-                    np.save("train_data_wcc2.npy", train_data)
-                    ser.write(b'5')
-                    keyRec.close()
-                    curses.nocbreak(); screen.keypad(0); curses.echo()
-                    curses.endwin()
-                    break
-                elif np.argmax(model.predict(image_np)) == 0 and dist > 100:
-                    ser.write(b'1')
-                    key = [1,0,0,0,0]
-                    print("moving forward")
-                elif np.argmax(model.predict(image_np)) == 1 and dist > 100:
-                    ser.write(b'2')
-                    key = [0,1,0,0,0]
-                    print("moving back")
-                elif np.argmax(model.predict(image_np)) == 2 and dist > 100:
-                    ser.write(b'3')
-                    key = [0,0,1,0,0]
-                    print("turning left")
-                elif np.argmax(model.predict(image_np)) == 3 and dist > 100:
-                    ser.write(b'4')
-                    key = [0,0,0,1,0]
-                    print("turning right")
-                elif np.argmax(model.predict(image_np)) == 4:
-                    ser.write(b'5')
-                    print("pausing")
-                    key = [0,0,0,0,1]
-                
-                #val_dict = {"input":key, "image":image_np}
-                #train_data.append(val_dict)
-                #keyRec.write(str(key)+"\n")
-                
-                #if len(train_data) % 100 == 0:
-                  #  np.save("train_data.npy", train_data)
-        #no pi camera, using USB
-        #else:
-         #   ret, image_np = cap.read()
-                
-          #  char = screen.getch()
-           # key = [0,0,0,0,1]
-            #if char == ord('x'):
-            #    np.save("train_data.npy", train_data)
-            #    ser.write(b'5')
-            #    keyRec.close()
-            #    curses.nocbreak(); screen.keypad(0); curses.echo()
-            #    curses.endwin()
-            #    break
-            #elif char == ord('w') and dist > 100:
-            #    ser.write(b'1')
-            #    key = [1,0,0,0,0]#
-
-            #elif char == ord('s') and dist > 100:
-            #    ser.write(b'2')
-            #    key = [0,1,0,0,0]
-                    
-            #elif char == ord('a') and dist > 100:
-            #    ser.write(b'3')
-            #    key = [0,0,1,0,0]
-                    
-            #elif char == ord('d') and dist > 100:
-            #    ser.write(b'4')
-            #    key = [0,0,0,1,0]
-            #elif char == ord(' '):
-            #    ser.write(b'5')
-            #    key = [0,0,0,0,1]
-                
-            #val_dict = {"input":key, "image":image_np}
-            #train_data.append(val_dict)
-            #keyRec.write(str(key)+"\n")
-                
-            #if len(train_data) % 100 == 0:
-             #   np.save("train_data.npy", train_data)
-finally:
-    #Close down curses properly, inc turn echo back on!
-    keyRec.close()
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
+            rawCapture.truncate(0)
+            result = model.predict([image_np])
+            print(result)        
