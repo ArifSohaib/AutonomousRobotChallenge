a4b7e7485fb2eed00ba87bddc78a7acc5f10c62c
Sohaib Arif <arif_sohaib@outlook.com>
2018-09-04 13:52:11 -0400

    added check for array of lidar readings instead of 1

--- motor_controls/lidarMoveTest.py
+++ motor_controls/lidarMoveTest.py
@@ -2,16 +2,21 @@ import serial
 
 serLidar = serial.Serial("/dev/ttyACM0","115200")
 serMotor = serial.Serial("/dev/ttyUSB0","9600")
-
+distArr = []
 while True:
     dist = serLidar.readline()
     try:
         dist = dist[:3]
         dist = int(dist)
+        distArr.append(dist)
+        if len(distArr) == 100:
+            del distArr[0]
     except ValueError:
-        print("can not convert '{}' to int".format(dist))
+        print("can not convert '{}' to int stopping".format(dist))
         serMotor.write(b'5')
-    if dist > 50:
+        exit()
+    distArrMore = [dist > 50 for dist in distArr]
+    if len(distArrMore) > 5:
         serMotor.write(b'1')
         print(dist)
         #print("moving forward")
