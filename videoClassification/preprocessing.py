e6fa346a842182243e4d41626ea6067736700a41
Sohaib <arif_sohaib@outlook.com>
2019-04-12 12:17:33 -0400

    added preprocessing functions

---
+++ videoClassification/preprocessing.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import pathlib
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [192, 192])
+    image /= 255.0  # normalize to [0,1] range
+    return image
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
+
+def caption_image(image_path, data_root):
+    image_rel = pathlib.Path(image_path).relative_to(data_root)
+    return "Image " + image_rel.parts[-1]
